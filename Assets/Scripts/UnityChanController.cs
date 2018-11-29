a85dca583c8d36afad20299c1ce27b333f38e28b
Ayaminn <ayamin.imai@gmail.com>
2018-11-29 14:22:28 +0900

    前後に動いてジャンプできるようなコードかいたよ

--- Assets/Scripts/UnityChanController.cs
+++ Assets/Scripts/UnityChanController.cs
@@ -4,13 +4,38 @@ using UnityEngine;
 
 public class UnityChanController : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    private Animator animator;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+
+            animator.SetBool("jumping", true);
+            animator.SetBool("walking", false);
+
+        }else if (Input.GetKey(KeyCode.UpArrow)){
+
+            transform.position += new Vector3(0, 0, 0.5f);
+            animator.SetBool("walking", true);
+            animator.SetBool("jumping", false);
+
+        }else if (Input.GetKey(KeyCode.DownArrow)){
+            
+            transform.position += new Vector3(0, 0, -0.5f);
+            animator.SetBool("walking", true);
+            animator.SetBool("jumping", false);
+
+        }else{
+
+            animator.SetBool("walking", false);
+            animator.SetBool("jumping", false);
+
+        }
+    }
 }
